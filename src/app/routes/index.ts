dc081b33ff706349be74e9c7bf77ec428600ff39
leoarafat10 <97153759+leoarafat10@users.noreply.github.com>
2023-08-21 20:32:52 +0800

    complete the file structure

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,12 +3,11 @@ import express from 'express';
 const router = express.Router();
 
 const moduleRoutes = [
-  // ... routes
   {
-    path: "",
-    routes: ""
-  }
+    path: '/user',
+    routes: 
+  },
 ];
 
-moduleRoutes.forEach(route => router.use(route.path, route.route));
+moduleRoutes.forEach(route => router.use(route.path, route.routes));
 export default router;
